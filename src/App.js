27aa7fedd25a002c96c71245a1706ff0254dd872
Peter <p.dyumin@dunice.net>
2019-11-06 11:30:04 +0300

    Final

--- src/App.js
+++ src/App.js
@@ -1,53 +1,53 @@
-import React, { useState, useCallback } from 'react';
-
-import './App.css';
+import React, { useCallback, useState } from 'react';
 
 function App() {
   const [todos, setTodos] = useState([]);
   
   const [input, handleInput] = useState('');
 
-  const addTodo = useCallback((input) => {
+  const addTodo = useCallback((event) => {
+    event.preventDefault();
     if (input) {
-      return setTodos([ ...todos, { id: Date.now(), todo: input } ]);
+      return setTodos(list => [ ...list, { done: false, id: Date.now(), todo: input } ]);
     }
-  }, [todos]);
+  }, [input]);
 
-  const deleteTodo = useCallback((id) => setTodos(todos.filter(todo => todo.id !== id)), [todos, setTodos]);
+  const deleteTodo = useCallback(id => setTodos(list => list.filter(item => item.id !== id)), []);
 
-  const handleChange = useCallback(value => handleInput(value), [handleInput]);
+  const handleChange = useCallback(event => handleInput(event.target.value), []);
+
+  const handleStatus = useCallback(
+    id => setTodos(list => list.map(item => item.id === id ? { ...item, done: !item.done } : item)),
+    [],
+  );
 
   return (
     <>
-      <div>TODO LIST</div>
+      <div>TO-DO LIST</div>
       <Form addTodo={addTodo} handleChange={handleChange} input={input} />
-      { todos.map(todo => <Todo key={todo.id} item={todo} deleteTodo={deleteTodo} />) }
+      { todos.map(todo => <Todo key={todo.id} item={todo} deleteTodo={deleteTodo} handleStatus={handleStatus} />) }
     </>
   );
 }
 
 const Form = React.memo((props) => {
-  console.log('form rendered', Date.now());
-  const addTodo = (e) => {
-    e.preventDefault();
-    return props.addTodo(props.input);
-  }
-
-  const handleChange = (e) => props.handleChange(e.target.value);
-
   return (
-    <form onSubmit={addTodo}>
-      <input type="text" onChange={handleChange} value={props.input} />
-      <button onClick={addTodo} type="submit">Add</button>
+    <form onSubmit={props.addTodo}>
+      <input type="text" onChange={props.handleChange} value={props.input} />
+      <button type="submit">Add</button>
     </form>
   );
 });
 
 const Todo = React.memo((props) => {
-  console.log('render todo', props.item.id, props.item.todo);
   return (
     <>
-      <div>{props.item.todo}</div>
+      <div
+        onClick={() => props.handleStatus(props.item.id)}
+        style={{ textDecoration: props.item.done ? 'line-through' : 'none' }}
+      >
+        {props.item.todo}
+      </div>
       <button onClick={() => props.deleteTodo(props.item.id)} type="button">Delete</button>
     </>
   );
